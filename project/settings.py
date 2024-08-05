032c35e1bb676341668f629dc1edae005726bfa5
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-08-05 14:13:57 +0300

    Django Crispy Forms

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'crispy_forms',
+    'bootstrap4',
     'core',
 ]
 
@@ -123,3 +125,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
